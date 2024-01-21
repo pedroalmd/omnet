4c89fda8fb6236c913fe1b97935c08137db927dc
pedro <pedro@omnetpp.myguest.virtualbox.org>
2024-01-21 17:58:12 -0300

    scripts

--- python_scripts/gen_values.py
+++ python_scripts/gen_values.py
@@ -7,15 +7,17 @@
 
 import random, string
 
-peer_amount = 5
+peer_amount = 200
 
 five_letters = ['a', 'b', 'c', 'd', 'e']
 
 amount_serving = []
-peers_serving = []
+peer_main_tcp = []
 contents_wants = []
 contents_has = []
 dying_time = []
+peers_serving = []
+
 
 for x in range(peer_amount):
     # if peer_amount <= 10:
@@ -23,7 +25,8 @@ for x in range(peer_amount):
     print(f"        switch[0].peer_gate++ <--> Channel <--> peer[{x}].gate++;")
 
     contents_wants.append(random.choice(five_letters))
-    contents_has.append(random.choice(five_letters))
+    tmp = five_letters.copy()
+    contents_has.append(random.choice(tmp))
 
     if random.randint(0,9) >= 7:
         dying_time.append(random.randint(5, 60))
@@ -33,16 +36,27 @@ for x in range(peer_amount):
         # continue
 
     amount_serving.append(0)
-    peers_serving.append(-1)
+    peer_main_tcp.append(-1)
+
+    tmp = '{' 
+    for x in range(peer_amount-1):
+        tmp += '-1'
+        if x == (peer_amount-2):
+            continue
+        tmp += ', '
+    tmp += '}'
+
+    peers_serving.append(tmp)
 
     # contents_wants.append(random.choice(string.ascii_lowercase))
     # contents_has.append(random.choice(string.ascii_lowercase))
     # dying_time.append(random.randint(5, 60))
 
 print(f"int amount_serving[PEER_AMOUNT] =  {{{(', '.join(str(x) for x in amount_serving))}}};")
-print(f"int peers_serving[PEER_AMOUNT] = {{{(', '.join(str(x) for x in peers_serving))}}};")
-
+print(f"int peers_serving[PEER_AMOUNT][PEER_AMOUNT - 1] = {{{(', '.join(str(x) for x in peers_serving))}}};")
+print("\n")
 print(f"int contents_wants[PEER_AMOUNT] = {{{str(contents_wants)[1:-1]}}};")
 print(f"int contents_has[PEER_AMOUNT] = {{{str(contents_has)[1:-1]}}};")
+print("\n")
 print(f"double peer_dying_time[PEER_AMOUNT] = {{{(', '.join(str(x) for x in dying_time))}}};")
-
+print(f"int peer_main_tcp[PEER_AMOUNT] = {{{(', '.join(str(x) for x in peer_main_tcp))}}};")
